a18b55e656fd428d2c4e532b74f058dfeec45df0
Kriper <ilinivan1602@gmail.com>
2022-05-23 19:51:39 +0300

    файлы сохраняются и называются(недоконца)

--- screenshot.py
+++ screenshot.py
@@ -1,6 +1,19 @@
 import pyautogui
 import datetime
+import time
+from datetime import date
+import os
+
 
 class Screenshoot():
     def __init__(self):
-        pyautogui.screenshot(f'{datetime.datetime.now()}.png')
+        today = str(date.today())
+        now = datetime.datetime.now()
+        now1 = now.hour
+        now2 = now.minute
+        now3 = now.second
+        os.makedirs("Screenshoot", exist_ok=True)
+        os.makedirs(os.path.join('Screenshoot', today), exist_ok=True)
+        pyautogui.screenshot(os.path.join('Screenshoot', today, f'{now1, now2,now3}.png'))
+        # pyautogui.screenshot(f'{datetime.datetime.now()}.png')
+
