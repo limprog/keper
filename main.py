af6ebf87ce79bf66e0c45d791b91901a32bc76d1
user <ilinivan1602gmail.com>
2022-05-26 18:26:06 +0300

    третьий аргумент (*расброс)

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--scr', default=5, type=int, help='Number of screenshots')
     parser.add_argument('--tscr', default=10, type=int, help='time between screenshots')
+    parser.add_argument('--var', default=0, type=int, help='scatter')
     args = parser.parse_args()
+    min = args.tscr - args.var
+    max = args.tscr + args.var
     for i in range(args.scr):
         screenshot_maker.screen()
-        time.sleep(args.tscr)
+        time.sleep(random.randint(min, max))
 
