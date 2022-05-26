6f4aad68011029e4279a5e7f8b271f65e1198d20
user <ilinivan1602gmail.com>
2022-05-26 18:19:56 +0300

    второй аргумет (*время)

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ if __name__ == '__main__':
     screenshot_maker = Screenshoot()
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--scr', default=5, type=int, help='Number of screenshots')
+    parser.add_argument('--tscr', default=10, type=int, help='time between screenshots')
     args = parser.parse_args()
     for i in range(args.scr):
         screenshot_maker.screen()
-        time.sleep(5)
+        time.sleep(args.tscr)
 
