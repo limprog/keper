7de89f035e75234b8631febddc89d76561fbd4e8
Kriper <ilinivan1602@gamail.com>
2022-06-02 14:33:11 +0300

    есть норм дата база

---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+import os
+
+
+os.makedirs("databas", exist_ok=True)
+df = sqlite3.connect(r'databas/keperdata.db')
+data = df.cursor()
+data.execute("""CREATE TABLE IF NOT EXISTS users(
+   userid INT PRIMARY KEY,
+   fname TEXT,
+   sname TEXT,
+   gender TEXT,
+   age INT);
+""")
+data.execute("""CREATE TABLE IF NOT EXISTS class(
+   classid INT PRIMARY KEY,
+   name TEXT,
+   grop TEXT,
+   chairmanid INT,
+   FOREIGN KEY (chairmanid) REFERENCES chairman(chairman));
+""")
+df.commit()
+data = df.cursor()
+data.execute("""CREATE TABLE IF NOT EXISTS users_class(
+    id INT  PRIMARY KEY,
+    userid INT,
+    classid INT,
+    FOREIGN KEY (userid) REFERENCES users(userid),
+    FOREIGN KEY (classid) REFERENCES class(classid));
+""")
+data.execute("""CREATE TABLE IF NOT EXISTS chairman(
+    chairmanid INT PRIMARY KEY,
+    userid INT,
+    classid INT,
+    FOREIGN KEY (userid) REFERENCES users(userid),
+    FOREIGN KEY (classid) REFERENCES class(classid));
+""")
+df.commit()
+
