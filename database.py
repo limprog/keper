64e65668f02246a802fa1e12fed076825b1cfd71
Kriper <ilinivan1602@gamail.com>
2022-09-05 18:41:19 +0300

    доделка

---
+++ database.py
@@ -0,0 +1,45 @@
+import sqlite3
+import os
+
+os.makedirs(os.path.join('web',"database"), exist_ok=True)
+df = sqlite3.connect(os.path.join("web/database/database.db"))
+data = df.cursor()
+data.execute("""CREATE TABLE IF NOT EXISTS users(
+   userid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   fname TEXT,
+   sname TEXT,
+   cod TEXT, 
+   email TEXT
+   );
+""")
+data.execute("""CREATE TABLE IF NOT EXISTS class(
+   classid INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+   name TEXT,
+   cod TEXT,
+   grop TEXT,
+   chairmanid INT,
+   FOREIGN KEY (chairmanid) REFERENCES chairman(chairman));
+""")
+
+df.commit()
+data = df.cursor()
+data.execute("""CREATE TABLE IF NOT EXISTS users_class(
+    id INT  PRIMARY KEY,
+    userid INT,
+    classid INT,
+    FOREIGN KEY (userid) REFERENCES users(userid),
+    FOREIGN KEY (classid) REFERENCES class(classid));
+""")
+data.execute("""CREATE TABLE IF NOT EXISTS chairman(
+    chairmanid INT PRIMARY KEY,
+    userid INT,
+    classid INT,
+    FOREIGN KEY (userid) REFERENCES users(userid),
+    FOREIGN KEY (classid) REFERENCES class(classid));
+""")
+data.execute("SELECT * FROM users;")
+print(data.fetchall())
+print(os.path.join('database.py'))
+df.commit()
+df.close()
+
